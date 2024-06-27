e5a2397bddbf183a626cadaac1be395b2c116743
kuldeeepkumar <pro.kuldeepkumar@gmail.com>
2024-06-27 17:08:59 +0530

    Activity Indicator component added and paper packages installed , some folder sturcture correction

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,45 +1,53 @@
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 
-import CustomButton from "../components/CustomButton";
-import React from "react";
+import CustomButton from "../component/CustomButton";
+import Loader from "../component/Loader";
 import { containerStyle } from "../styles";
 
 const WelcomeImage = require("../../assets/img/welcome.jpg");
 
 const Welcome = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
-    <ImageBackground
-      imageStyle={{ opacity: 0.7 }}
-      source={WelcomeImage}
-      style={[containerStyle.container, {}]}>
-      <Text
-        style={{
-          fontSize: 100,
-          fontWeight: "bold",
-          color: "black",
-          fontFamily: "Inter-Black",
-          backgroundColor: "yellow",
-          paddingHorizontal: 10,
-        }}>
-        NEWS
-      </Text>
-      <Text
-        style={{
-          fontSize: 45,
-          fontWeight: "bold",
-          color: "black",
-          fontFamily: "Inter-Black",
-          borderColor: "yellow",
-          borderWidth: 2,
-          padding: 12,
-        }}>
-        READER APP
-      </Text>
+    <>
+      {isLoading === false ? (
+        <ImageBackground
+          imageStyle={{ opacity: 0.7 }}
+          source={WelcomeImage}
+          style={[containerStyle.container, {}]}>
+          <Text
+            style={{
+              fontSize: 48,
+              fontWeight: "bold",
+              color: "black",
+              fontFamily: "Inter-Black",
+              backgroundColor: "yellow",
+              paddingHorizontal: 10,
+            }}>
+            LANGUAGE
+          </Text>
+          <Text
+            style={{
+              fontSize: 30,
+              fontWeight: "bold",
+              color: "black",
+              fontFamily: "Inter-Black",
+              borderColor: "yellow",
+              borderWidth: 2,
+              padding: 12,
+            }}>
+            TRANSLATOR APP
+          </Text>
 
-      <View style={{ marginTop: 200 }}>
-        <CustomButton title={"Get Started"}></CustomButton>
-      </View>
-    </ImageBackground>
+          <View style={{ marginTop: 200 }}>
+            <CustomButton title={"Get Started"}></CustomButton>
+          </View>
+        </ImageBackground>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 
